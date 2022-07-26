598e152d62804f22aa8222dbf486c7b5690a317e
MasterplaYCoding <mateiursache2710@gmail.com>
2022-07-26 22:50:14 +0300

    Info page header

--- pages/tipsAndTricks/tipsAndTricks.js
+++ pages/tipsAndTricks/tipsAndTricks.js
@@ -1,21 +1,42 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button, Image} from 'react-native';
 
 
 export function TipsAndTricks() {
   return (
     <View style={styles.container}>
-      <Text>Ba merge aplicatia!</Text>
+      <View>
+        <Text style = {styles.welcomeback}>Welcome back,</Text>
+        <View style = {styles.davidContainer}>
+          <Text style = {styles.david}>David!</Text>
+          <Image source = {require('../../assets/charging 1.png')} style = {styles.charging1}   />
+        </View>     
+      </View>
+      <View></View>
     </View>
   );
 }
-/*Salut Matei, mai jos poti crea stiluri pt orice element, ca sa nu le scrii chiar la 
-view-uri. astea ajuta la refolosirea codului
-*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50
   },
+  welcomeback: {
+    color: 393939,
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  david: {
+    color: 393939,
+    fontSize: 27,
+  },
+  davidContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  charging1: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  }
 })
