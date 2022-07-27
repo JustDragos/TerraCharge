726b3f76e8573e3998b141388a22a7f0689564a0
MasterplaYCoding <mateiursache2710@gmail.com>
2022-07-27 17:50:39 +0300

    Tips and tricks text

--- pages/tipsAndTricks/tipsAndTricks.js
+++ pages/tipsAndTricks/tipsAndTricks.js
@@ -1,25 +1,72 @@
 import { Text, View, StyleSheet, Button, Image} from 'react-native';
+import { back } from 'react-native/Libraries/Animated/Easing';
 
 
 export function TipsAndTricks() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.firstContainer}>
         <Text style = {styles.welcomeback}>Welcome back,</Text>
         <View style = {styles.davidContainer}>
           <Text style = {styles.david}>David!</Text>
           <Image source = {require('../../assets/charging 1.png')} style = {styles.charging1}   />
         </View>     
       </View>
-      <View></View>
+      <View style = {styles.secoundContainer}>
+        <Text style = {styles.mostPopular}>Most popular</Text>
+        <View style={{    backgroundColor: '#41EF22',}}>
+          <View style = {styles.tipsContainer}>
+            <Text style = {styles.tips}>Tips and tricks</Text>
+            <Text style  = {styles.battery}>for battery health</Text>
+            <Button title='see now'/>
+          </View>
+          <View>
+            <Image source = {require('../../assets/question (1) 1.png')}/>
+          </View>
+        </View>
+      </View>
+      <View style = {styles.thirdContainer}>
+        <View>
+          <Text>Others</Text>
+          <Button title = 'View all'/>
+        </View>
+        <View>
+
+        </View>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingBottom: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignContent: 'center',
+    width: 400,
+    height: 600,
+    backgroundColor: '#fff'
+  },
+  firstContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    padding: 50
+    paddingBottom: 10
+  },
+  secoundContainer: {
+    flex: 1,
+    paddingBottom: 50
+  },
+  thirdContainer: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  tipsContainer: {
+    paddingTop: 5,
+    justifyContent: 'flex-start',
+    alignContent: 'stretch',
+    paddingLeft: 20
   },
   welcomeback: {
     color: 393939,
@@ -36,7 +83,25 @@ const styles = StyleSheet.create({
   },
   charging1: {
     flex: 1,
-    width: '100%',
-    height: '100%',
+    width: '200%',
+    height: '200%',
+    resizeMode: 'center'
+  },
+  mostPopular: {
+    color: 989898,
+    fontSize: 15
+    
+  },
+  seeNow: {
+
+  },
+  tips: {
+    color: 393939,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  battery: {
+    color: 393939,
+    fontSize: 18,
   }
 })
