33197734907fa3fb514b54d1db161df8b9d7b866
MasterplaYCoding <mateiursache2710@gmail.com>
2022-08-22 16:12:16 +0300

    profiles update

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -7,22 +7,28 @@ export function Profiles() {
             <View style = {styles.firstContainer}>
                 <View>
                     <Pressable>
-                        <Image source = {require('../../assets/navigators/go_back.png')}/>
+                        <Image source = {require('../../assets/navigators/go_back.png')} style = {styles.backIcon}/>
                     </Pressable>
                 </View>
-                <View>
-                    <Image source={require('../../assets/logo/dpitLogo.png')}/>
+                <View style = {{paddingLeft: 200}}>
+                    <Image source = {require('../../assets/logo/dpitLogo.png')} style = {styles.backIcon}/>
                 </View>
             </View>
             <View style = {styles.secoundContainer}>
-                <View>
-
+                <View style = {{flex: 1, paddingRight: 30}}>
+                    <Image source = {require('../../assets/emote.png')} style = {{width: 100, height: 100}}/>
                 </View>
-                <View style = {{flexDirection: 'column'}}>
-                    <Text>Welcome</Text>
-                    <Text>Mister David</Text>
+                <View style = {{flexDirection: 'column', flex: 2, justifyContent: 'center'}}>
+                    <View style = {{paddingBottom: 6}}>
+                        <Text style = {{color: '#828282', fontSize: 20}}>Welcome</Text>
+                    </View>
+                    <View>
+                        <Text style = {{color: '#000000', fontSize: 20, fontWeight: '600'}}>Mister David</Text>
+                    </View>
                 </View>
-                <View></View>
+                <View style = {{flex: 1, justifyContent: 'center'}}>
+                    <Image source = {require('../../assets/fi_log-out.png')} style = {{width: 40, height: 40}}/>
+                </View>
             </View>
             <View></View>
             <View></View>
@@ -36,15 +42,26 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignContent: 'center',
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        paddingLeft: 30,
+        paddingRight: 30
     },
     firstContainer: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        paddingBottom: 7
     },
     secoundContainer: {
         flexDirection: 'row',
-        borderBottomColor: '#989898',
-        borderTopColor: '#989898'
+        borderBottomColor: '#828282',
+        borderBottomWidth: 1,
+        borderTopColor: '#828282',
+        borderTopWidth: 1,
+        paddingLeft: 15
+    },
+    backIcon: {
+        resizeMode: 'center',
+        width: 60,
+        height: 60
     }
 })
 
